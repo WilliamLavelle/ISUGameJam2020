f8accecb9ceaf6a68e4d65a174941b8998b9462c
cjdupuis1 <69912232+cjdupuis1@users.noreply.github.com>
2020-11-07 16:15:34 -0600

    Add files via upload

---
+++ Weapon1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon1 : MonoBehaviour
+{
+   public GameObject bulletPrefab;
+   private Transform playerPos;
+
+   void Start()
+   {
+   	playerPos = GetComponent<Transform>();
+   }
+
+   void Update()
+   {
+   	if(Input.GetButtonDown("Fire1"))
+   	{
+   		Shoot();
+   	}
+   }
+
+   void Shoot()
+   {
+      Instantiate(bulletPrefab, playerPos.position, Quaternion.identity);
+   }
+}
